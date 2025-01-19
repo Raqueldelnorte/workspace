60143f6493450f3f75a164299e662a9f645efc79
RaqueldelNorte <rgonic@gmail.com>
2025-01-19 16:04:33 +0100

    creados los dos campos en la bbdd email y active

--- express/class5/app.js
+++ express/class5/app.js
@@ -3,10 +3,26 @@ const students = require('./repositories/students');
 
 const app = express();
 const port = 3000;
+
 app.use(express.json());
+// Obtener todos los estudiantes
 app.get('/students', (req, res) => {
   students.getAll().then((results) => res.json(results));
 });
+
+// Obtener un estudiante por ID
+app.get('/students/:id', (req, res) => {
+  const studentId = req.params.id;
+  students.getById(studentId).then((student) => {
+    if (!student) {
+      return res.status(404).send('Student doesn’t exist');
+    }
+    res.json(student);
+  }).catch((err) => {
+    res.status(500).send('Internal server error');
+  });
+});
+// Crear un nuevo estudiante
 app.post('/students', (req, res) => {
   if (!(req.body.name && req.body.last_name && req.body.date_of_birth)) {
     res.status(422).send('All fields are required (name, last_name, date_of_birth)');
@@ -23,6 +39,10 @@ app.post('/students', (req, res) => {
         res.json({ success: false, message: err.detail });
       });
   }
+});
+// Middleware para manejar rutas no encontradas
+app.use((req, res, next) => {
+  res.status(404).send('Route not found');
 });
 
 app.listen(port, () => {
---
+++ express/class5/migrations/20250119145455-add-email-and-active-to-students.js
@@ -0,0 +1,23 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.addColumn('students', 'email', {
+      type: Sequelize.STRING,
+      allowNull: true, // Puedes poner `false` si es obligatorio
+    })
+      .then(() => {
+        return queryInterface.addColumn('students', 'active', {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: true, // Valor predeterminado
+        });
+      });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.removeColumn('students', 'email')
+      .then(() => {
+      return queryInterface.removeColumn('students', 'active');
+      });
+  },
+};
