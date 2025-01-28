c138cf34fde38f47780abc1e41bd28310fa05352
RaqueldelNorte <rgonic@gmail.com>
2025-01-28 12:02:01 +0100

    app con create-react-app modificando App.js

--- react/clase1/my-second-app/src/App.js
+++ react/clase1/my-second-app/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  const name = 'Fausto López';
-  return <h1>Hello {name}</h1>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a mi Aplicación React</h1>
+        <p>Esta es una modificación de App.js.</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
