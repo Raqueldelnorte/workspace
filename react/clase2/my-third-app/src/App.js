048a9ba9aa0507ca88fe060139cef8bea35b55a1
RaqueldelNorte <rgonic@gmail.com>
2025-01-28 17:41:45 +0100

    ejercicios de la clase2

---
+++ react/clase2/my-third-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Definimos el componente App
+function App() {
+  return <h1>Hello World!</h1>;
+}
+
+// Exportamos el componente para usarlo en otros archivos
+export default App;
