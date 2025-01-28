8759696456529911fe372901c555dd63203741a9
RaqueldelNorte <rgonic@gmail.com>
2025-01-28 12:06:15 +0100

    cambios de estilo y añadido algún elemento

--- react/clase1/my-second-app/src/App.js
+++ react/clase1/my-second-app/src/App.js
@@ -5,8 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bienvenido a mi Aplicación React</h1>
-        <p>Esta es una modificación de App.js.</p>
+        {/* Título principal */}
+        <h1>Hello World!</h1>
+
+        {/* Descripción */}
+        <p>Este es el primer paso para crear una aplicación React.</p>
+
+        {/* Botón con estilo */}
+        <button className="App-button" onClick={() => alert('¡Hola desde el botón!')}>
+          Haz clic aquí
+        </button>
+
+        {/* Texto adicional */}
+        <p>Este es un elemento extra que se ha añadido.</p>
       </header>
     </div>
   );
