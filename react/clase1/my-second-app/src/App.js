18f777a920d1fa09f9845970a05fa33db4399a60
RaqueldelNorte <rgonic@gmail.com>
2025-01-28 11:54:11 +0100

    con método estático

---
+++ react/clase1/my-second-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function App() {
+  const name = 'Fausto López';
+  return <h1>Hello {name}</h1>;
+}
+
+export default App;
--- react/clase1/my-second-app/src/index.js
+++ react/clase1/my-second-app/src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';  // Asegúrate de que está importando index.css
-
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello World!</h1>  // Renderiza "Hello World!"
-    );
-  }
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
