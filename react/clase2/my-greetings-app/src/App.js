cb9b44433774e314583a61bbc09137bd8dd8b863
RaqueldelNorte <rgonic@gmail.com>
2025-02-01 09:37:22 +0100

    App que acepta una prop name con un texto y renderiza tres bienvenidas con diferentes nombres

--- react/clase2/my-greetings-app/src/App.js
+++ react/clase2/my-greetings-app/src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import Welcome from './Welcome';
+import React from "react";
+import Welcome from "./Welcome";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Saludos Personalizados</h1>
-        <Welcome name="Raquel" />
-        <Welcome name="Luisa" />
-        <Welcome name="Mario" />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Saludos Personalizados</h1>
+      <Welcome name="Raquel" />
+      <Welcome name="Teresa" />
+      <Welcome name="Lucía" />
+    </div>
+  );
+};
 
 export default App;
+
