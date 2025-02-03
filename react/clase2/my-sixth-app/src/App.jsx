d029e6b0dbdd6c14c514eaad20fe1c3b89062678
RaqueldelNorte <rgonic@gmail.com>
2025-02-03 17:16:48 +0100

    cambios de programación de código y de estilo

--- react/clase2/my-sixth-app/src/App.jsx
+++ react/clase2/my-sixth-app/src/App.jsx
@@ -1,19 +1,39 @@
-//Esta App.js lo he creado con Vite
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
-function App() {
+const FancyBorder = (props) => {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
     </div>
   );
-}
+};
+
+const WelcomeDialog = () => {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">Bienvenido!</h1>
+      <p className="Dialog-message">Explora todos nuestros servicios!</p>
+    </FancyBorder>
+  );
+};
+
+const GoodByeDialog = () => {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">Adios!</h1>
+      <p className="Dialog-message">Gracias por visitar nuestra web!</p>
+    </FancyBorder>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <WelcomeDialog />
+      <GoodByeDialog />
+    </div>
+  );
+};
 
 export default App;
